21bd439517352c5c795ca6497071cd37001d07d0
LizzIna <valentinacalderon466@gmail.com>
2026-09-06 18:30:52 -0300

    creandoa rchivo y carpeta para linkns

--- src/components/home/HomeCards.jsx
+++ src/components/home/HomeCards.jsx
@@ -1,28 +1,19 @@
 export default function HomeCards({title, description, icon}) {
     return (
-        
-        // Cards Reutilizables para Home 
         <div className="flex flex-col h-50 w-100 bg-[#314139] rounded-2xl p-7 max-w-full max-sm:w-full max-sm:h-auto max-sm:p-5">
             
-            {/* fila superior: titulo y icono */}
-            <div className=" flex items-center justify-between h-1/2 p-5 max-sm:p-3">
-                
-                <h3 className="text-2xl font-bold text-[#FDFBF7]   max-sm:text-xl">{title}</h3>
-                <div className=" font-bold flex h-7 w-7 items-center justify-center mr-10 max-sm:mr-4">
+            {/* fila superior: icono y titulo */}
+            <div className="flex items-center gap-4 h-1/2 p-5 max-sm:p-3">
+                <div className="font-bold flex h-12 w-12 items-center justify-center text-[#D4AF37] max-sm:h-10 max-sm:w-10">
                     {icon}
                 </div>
+                <h3 className="text-2xl font-bold text-[#FDFBF7] max-sm:text-xl">{title}</h3>
             </div>
 
-            {/* fila inferior: icono y descripcion*/}
-            <div className=" flex items-end justify-between h-1/2 ">
-
-                <div  aria-hidden="true " className=" flex h-5 w-5 items-center justify-center max-w-1/4 font-bold p-5 ml-10 max-sm:ml-4 max-sm:p-3">
-                    {icon}
-                </div>
-                <p className=" font-semibold text-sm text-[#A8B0AB] justify-center  ">{description} </p>
-            
+            {/* fila inferior: descripcion */}
+            <div className="flex items-end justify-between h-1/2">
+                <p className="font-semibold text-sm text-[#A8B0AB] justify-center">{description}</p>
             </div>
         </div>
     );
-
 };
